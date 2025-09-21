a5101082cb68920515599dde7b87aabc87a9a09a
Sun Weilin <weilin.sun@shopee.com>
2025-09-21 21:43:41 +0800

    33

--- src/hello.go
+++ src/hello.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+type IAttack interface {
+	Attack()
+}
+
+type Warrior struct {
+	Name string
+}
+
+type Empire struct {
+	Name string
+}
+
+func (w Warrior) Attack() {
+	fmt.Printf("%s attacks with a sword!\n", w.Name)
+}
+
+func (e Empire) Attack() {
+	fmt.Printf("%s attacks with a legion!\n", e.Name)
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	var attacker IAttack
+	attacker = Warrior{Name: "Conan"}
+	attacker.Attack()
+
+	attacker = Empire{Name: "Roman Empire"}
+	attacker.Attack()
 }
