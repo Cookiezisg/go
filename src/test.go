3431c49bbeb2d7eadeeac7a8fb40d17760ca53ad
Sun Weilin <weilin.sun@shopee.com>
2025-10-27 00:15:27 +0800

    e

--- src/test.go
+++ src/test.go
@@ -12,6 +12,8 @@ type Person struct {
 func main() {
 	t := template.New("fieldname example")
 	t, _ = t.Parse("hello {{.UserName}}!")
+
+	println(t.Tree.Root.String())
 	p := Person{UserName: "Weilin Sun"}
 	t.Execute(os.Stdout, p)
 }
